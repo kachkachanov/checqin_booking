cad8694f880ae1cb455bb0dd76763a7dae73e338
kachkachanov <Kachanov0b@gmail.com>
2026-03-25 20:52:39 +0300

    Remove inline comments from app templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,18 @@
 Rails.application.routes.draw do
-  # Devise для аутентификации
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
-
-  # Главная страница
   root 'hotels#index'
-
-  # Поиск отелей
   get 'search', to: 'hotels#search', as: :search_hotels
-
-  # Избранное
   resource :favorites, only: [:show] do
     post 'toggle/:hotel_id', to: 'favorites#toggle', as: :toggle
   end
-
-  # Отели и номера
   resources :hotels do
     resources :rooms
   end
-
-  # Жильё целиком
   resources :properties, only: [:show]
-
-  # Супервайзор
   namespace :supervisor do
     root 'dashboard#index'
     get 'choice', to: 'dashboard#choice'
