15689c62eaeb7c466531512372f396e20f08eebe
babenko19igor99-cmd <babenko19.igor99@gmail.com>
2026-03-26 22:45:45 +0300

    added email validation, need two_factor_aut

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,21 @@
 class User < ApplicationRecord
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
   has_many :hotels, foreign_key: :user_id, dependent: :nullify
   has_many :properties, dependent: :nullify
   has_many :favorites, dependent: :destroy
   has_many :favorite_hotels, through: :favorites, source: :hotel
+
+  # Кастомная валидация для почты
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,
+    presence: true,
+    uniqueness: {case_sensitive: false},
+    format: { with: VALID_EMAIL_REGEX, message: "incorrect email" }
 
   def supervisor?
     role == 'supervisor'
